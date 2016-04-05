6ba45c440ce5f2c928c9ffe596bbdf4ba088fe30
Daan <dpdaan@hotmail.com>
2016-04-05 12:33:47 +0200

    Gerroe

--- test/group2/keybarricade/LevelLoaderTest.java
+++ test/group2/keybarricade/LevelLoaderTest.java
@@ -1,11 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package group2.keybarricade;
 
-import java.util.ArrayList;
+import java.io.InputStream;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -13,27 +8,23 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-/**
- *
- * @author Wouter
- */
 public class LevelLoaderTest {
-    
+
     public LevelLoaderTest() {
     }
-    
+
     @BeforeClass
     public static void setUpClass() {
     }
-    
+
     @AfterClass
     public static void tearDownClass() {
     }
-    
+
     @Before
     public void setUp() {
     }
-    
+
     @After
     public void tearDown() {
     }
@@ -42,14 +33,37 @@ public class LevelLoaderTest {
      * Test of loadLevels method, of class LevelLoader.
      */
     @Test
-    public void testLoadLevels() {
-        System.out.println("loadLevels");
+    public void testLoadLevels1() {
         LevelLoader instance = new LevelLoader();
-        ArrayList<PlayField> expResult = null;
-        ArrayList<PlayField> result = instance.loadLevels();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        int expResult = 7;
+        int result = instance.loadLevels().size();
+        assertEquals("#loadLevels - Normal Load Test", expResult, result);
     }
-    
+
+    /**
+     * Test of loadLevel method, of class LevelLoader.
+     */
+    @Test
+    public void testLoadLevel1() {
+        InputStream input = null;
+        LevelLoader instance = new LevelLoader();
+        PlayField expResult = null;
+        PlayField result = instance.loadLevel(input);
+        assertEquals("#loadLevel - Null Test", expResult, result);
+    }
+
+    @Test
+    public void testLoadLevel2() {
+        InputStream input = getInputStream("/test/testfiles/nonjson.yml");
+        System.out.println("");
+        LevelLoader instance = new LevelLoader();
+        PlayField expResult = null;
+        PlayField result = instance.loadLevel(input);
+        assertEquals("#loadLevel - Non json test", expResult, result);
+    }
+
+    private InputStream getInputStream(String path) {
+        return getClass().getResourceAsStream(path);
+    }
+
 }
